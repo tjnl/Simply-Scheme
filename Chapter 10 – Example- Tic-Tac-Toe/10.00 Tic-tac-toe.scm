23860b1286bc949232287913a046bd91625f01d3
tjnl <thomas.liggett@gmail.com>
2020-01-07 12:02:28 +1300

    Add chapter 10

---
+++ Chapter 10 – Example- Tic-Tac-Toe/10.00 Tic-tac-toe.scm
@@ -0,0 +1,142 @@
+;;; ttt.scm
+;;; Tic-Tac-Toe program
+
+(define (ttt position me)
+  (ttt-choose (find-triples position) me))
+
+(define (find-triples position)
+  (every (lambda (comb) (substitute-triple comb position))
+         '(123 456 789 147 258 369 1245 2356 4578 5689 159 357)))
+
+(define (substitute-triple combination position)
+  (accumulate word
+	      (every (lambda (square)
+		       (substitute-letter square position))
+		     combination) ))
+
+(define (substitute-letter square position)
+  (if (equal? '_ (item square position))
+      square
+      (item square position) ))
+
+(define (ttt-choose triples me)
+  (cond ((i-can-win? triples me))
+        ((opponent-can-win? triples me))
+        ((i-can-fork? triples me))
+        ((i-can-advance? triples me))
+        (else (best-free-square triples))))
+
+(define (i-can-win? triples me)
+  (choose-win
+   (keep (lambda (triple) (my-pair? triple me))
+         triples)))
+
+(define (my-pair? triple me)
+   (and (>= (appearances me triple) 2)
+        (= (appearances (opponent me) triple) 0)))
+  
+
+(define (opponent letter)
+  (if (equal? letter 'x) 'o 'x))
+
+(define (choose-win winning-triples)
+  (if (empty? winning-triples)
+      #f
+      (keep number? (first winning-triples)) ))
+
+(define (opponent-can-win? triples me)
+  (i-can-win? triples (opponent me)) )
+
+(define (i-can-fork? triples me)
+  (first-if-any (pivots triples me)) )
+
+(define (first-if-any sent)
+  (if (empty? sent)
+      #f
+      (first sent) ))
+
+(define (pivots triples me)
+  (repeated-numbers (keep (lambda (triple) (my-single? triple me))
+                          triples)))
+
+(define (my-single? triple me)
+  (and (= (appearances me triple) 1)
+       (= (appearances (opponent me) triple) 0)))
+
+(define (repeated-numbers sent)
+  (every first
+         (keep (lambda (wd) (>= (count wd) 2))
+               (sort-digits (accumulate word sent)) )))
+
+(define (sort-digits number-word)
+  (every (lambda (digit) (extract-digit digit number-word))
+         '(1 2 3 4 5 6 7 8 9) ))
+
+(define (extract-digit desired-digit wd)
+  (keep (lambda (wd-digit) (equal? wd-digit desired-digit)) wd))
+
+(define (i-can-advance? triples me)
+  (best-move (keep (lambda (triple) (my-single? triple me)) triples)
+             triples
+             me))
+
+(define (best-move my-triples all-triples me)
+  (if (empty? my-triples)
+      #f
+      (best-square (first my-triples) all-triples me) ))
+
+(define (best-square my-triple triples me)
+  (best-square-helper (pivots triples (opponent me))
+		      (keep number? my-triple)))
+
+(define (best-square-helper opponent-pivots pair)
+  (if (member? (first pair) opponent-pivots)
+      (first pair)
+      (last pair)))
+
+(define (best-free-square triples)
+  (first-choice (accumulate word triples)
+                '(5 1 3 7 9 2 4 6 8)))
+
+(define (first-choice possibilities preferences)
+  (first (keep (lambda (square) (member? square possibilities))
+               preferences)))
+
+
+;; Solutions to excercises
+
+;; 10.1
+
+(define (already-won? position me)
+  (member? (word me me me) (find-triples position)))
+
+;; 10.2
+
+(define (tie-game? position)
+  (not (member? '_ position)))
+
+;; 10.3
+
+(define (tie-game? position)
+  (let ((count-empty-squares (count (keep (lambda (x) (equal? '_ x)) position)))
+        (count-dud-triples (count (keep (lambda (triple) (dud-triple? triple)) (find-triples position)))))
+    (or (and (= count-empty-squares 2)
+             (= count-dud-triples 2))
+        (and (= count-empty-squares 1)
+             (= count-dud-triples 3))
+        (= count-empty-squares 0)))) ;; incorporate the definition from 10.2
+
+(define (dud-triple? triple)
+  (and (= (appearances 'x triple) 1)
+       (= (appearances 'o triple) 1)))
+
+#| Tests for correctness of 10.3
+(tie-game? 'oxooxxxo_) ;; should show #t
+(tie-game? 'xoxxoxo_o) ;; should show #f, as we don't know whose turn it is
+(tie-game? 'xo_x_xoox) ;; should show #f
+(tie-game? 'x_xxxoo_o) ;; should show #f
+(tie-game? 'oxo_x_xox) ;; should show #t
+(tie-game? 'x__xoxoox) ;; should show #f
+(tie-game? 'oxoxoxxo_) ;; should show #f, as we don't know whose turn it is
+(tie-game? 'oxooxox_x) ;; should show #f, as we don't know whose turn it is
+|#
